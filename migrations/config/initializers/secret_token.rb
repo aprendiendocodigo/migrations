45d8be3393fb655b246441f10e77999d1b8f8475
Richard <rbarrera87@gmail.com>
2014-01-22 00:36:19 -0800

    first commit of the exercise

---
+++ migrations/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Migrations::Application.config.secret_key_base = 'f2b24513c3bee94795b866de6c91a8135c0ea96e3ff01ef8f859546e2d38848889f24c63517bca76d7d8958d1bc684ea3062fbef08f86c188be4ea5123a6490c'
